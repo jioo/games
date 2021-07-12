0119fc3d00578623ee833870e8e40751e28e4c07
jioo <jio.qzn@gmail.com>
2021-07-12 14:33:58 +0000

    Deploying to gh-pages from  @ ff7ed518ed28270d92f3b13e47f0e29ff7511842 🚀

--- _nuxt/static/1612957096/castlevania-the-dracula-x-chronicles/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1612957096",layout:"default",error:null,state:{params:{token:"sGQamtNrNnpeyp0ac7AuGAtt",cv:1612957058}},serverRendered:true,routePath:"\u002Fcastlevania-the-dracula-x-chronicles",config:{}};
---
+++ _nuxt/static/1626100306/gallery/freestyle-mobile-ph/782942/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__={staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1626100306",layout:"default",error:null,state:{params:{token:"sGQamtNrNnpeyp0ac7AuGAtt",cv:1613931340}},serverRendered:true,routePath:"\u002Fgallery\u002Ffreestyle-mobile-ph\u002F782942",config:{}};
